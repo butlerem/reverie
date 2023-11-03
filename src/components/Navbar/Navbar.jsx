a5c84e88b2f5ba2ff34e5e101a833b9b511ab4a4
butlerem <butlerem01@gmail.com>
2023-11-03 14:32:58 -0400

    base css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,6 +23,22 @@ const Navbar = () => (
       <div />
       <a href="/" className="p__opensans">Book a Table</a>
     </div>
+    <div className="app__navbar-smallscren_overlay flex">
+      <GiHamburgerMenu color= "#fff" fontSize={27} onClick={() => {}}/>
+
+      <div className='app__navbar-smallscren_overlay flex__center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}}/>
+      <ul className= "app__navbar-smallscren-links">
+       <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#reservations">Reservations</a></li>
+       <li className="p__opensans"><a href="#careers">Careers</a></li>
+      </ul>
+      </div>
+
+    </div>
   </nav>
 );
 
