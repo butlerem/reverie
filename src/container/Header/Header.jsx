ded16e75f702bc0b1a9722d56c010af2b8ceb3f4
butlerem <butlerem01@gmail.com>
2023-11-06 14:54:15 -0500

    subheader and about sections

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,9 +7,9 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <SubHeading title="Elegant Dining from Sunrise to Sunset" />
+      <h1 className="app__header-h1">Where Culinary Dreams Begin</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Reverie is where culinary dreams come to life. We believe that each sunrise and sunset deserves to be celebrated with the finest flavors. We're here to make your dining experiences truly unforgettable. </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
