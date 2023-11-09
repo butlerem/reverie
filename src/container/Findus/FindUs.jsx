c5fcb7140cf80229cf6352f7d8f5ef18695fb2dc
butlerem <butlerem01@gmail.com>
2023-11-09 16:16:49 -0500

    imbed google map

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 
@@ -9,7 +8,7 @@ const FindUs = () => (
       <SubHeading title="Contact" />
       <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
       <div className="app__wrapper-content">
-        <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
+        <p className="p__opensans">165 Chapel St Unit No. 1, Ottawa, ON K1N 0E7</p>
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
         <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
         <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
@@ -18,7 +17,16 @@ const FindUs = () => (
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.findus} alt="finus_img" />
+
+      <iframe
+    width="100%"
+    height="300"
+    frameBorder="0"
+    style={{ border: '3px solid #f7c8db' }}
+        src={`https://www.google.com/maps/embed/v1/view?key=AIzaSyBhaUYpq1VXktczprP467rfPV43W6GBA3o&center=45.43120532869718, -75.68066586758836&zoom=15&maptype=roadmap`}
+        allowFullScreen
+        title="Google Map"
+      ></iframe>
     </div>
   </div>
 );
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,7 +7,7 @@ import './SpecialMenu.css';
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits your palatte" />
+      <SubHeading title="Crafting Culinary Dreams for you" />
       <h1 className="headtext__cormorant">Today's Specials</h1>
     </div>
 
