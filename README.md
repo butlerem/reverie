14c85e8ce5de5e95fec2394afcfd73bebbe80d81
butlerem <butlerem01@gmail.com>
2023-11-11 03:00:17 -0500

    update readme and styling

---
+++ README.md
@@ -0,0 +1,66 @@
+# Restaurant Website Project
+
+This repository showcases a restaurant website that I've developed to demonstrate my skills as a web developer, with a focus on React and UI/UX design. The project aims to provide an elegant and engaging online presence for a fictional restaurant called "Reverie."
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies Used](#technologies-used)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+Welcome to the Reverie restaurant website project! This project serves as a testament to my proficiency in building interactive and visually appealing user interfaces using React. The website is designed to showcase the restaurant's ambiance, menu offerings, and key information, providing users with an immersive experience.
+
+## Features
+
+- **Homepage:** Elegant introduction with a welcome message and a call-to-action button.
+- **About Us:** Detailed information about the restaurant, its history, and the vision behind it.
+- **Menu:** A mouthwatering display of breakfast and cocktail offerings, complete with descriptions and prices.
+- **Chef's Word:** A personal message from the chef and founder, Emma Butler, emphasizing the restaurant's culinary philosophy.
+- **Awards:** Recognition and accolades received by the restaurant for its dedication to excellence.
+- **Gallery:** A visually appealing gallery showcasing the restaurant's ambiance and dishes.
+- **Contact:** Information about the restaurant's location, opening hours, and a contact form.
+- **Newsletter:** A subscription feature for users to stay updated with the latest news and offerings.
+
+## Installation
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/restaurant-website.git
+
+2. Navigate to the project directory:
+
+   ```bash
+    cd restaurant-website
+
+ 
+3. Install the required dependencies:
+
+   ```bash
+    npm install
+
+
+## Usage
+Start the development server:
+
+   ```bash
+    npm start
+
+    
+Open your browser and go to http://localhost:3000 to view the restaurant website.
+
+## Technologies Used
+React
+HTML5
+CSS3
+JavaScript
+
+
+## License
+This project is licensed under the MIT License.
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -10,14 +10,14 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans text-center">Reverie, the epitome of elegance and culinary innovation, has redefined the art of fine dining for breakfast aficionados and cocktail lovers alike. Established in 2023 by Executive pastry chef Emma Butler, Reverie was born from her vision of a dream restaurant, a captivating and airy space that brings breakfast to new heights. Within the welcoming ambiance, guests are treated to delectable breakfast items and visually stunning cocktails, making every visit an unforgettable culinary journey.</p>
+        <p className="p__opensans text-center">Reverie has redefined the art of fine dining for breakfast aficionados and cocktail lovers alike. Established in 2023 by Executive pastry chef Emma Butler, Reverie was born from the vision of a dream restaurant, a captivating and airy space that brings breakfast to new heights. Within the welcoming ambiance, guests are treated to delectable breakfast items and visually stunning cocktails, making every visit an unforgettable culinary journey.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
 
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans text-center">In 2023, Emma's dream of Reverie came to life as she envisioned an upscale breakfast haven. This picturesque establishment blends elegance with the joy of comfort food. Reverie has become a place where breakfast transcends the ordinary. Our history is a testament to her vision, with each day adding to the legacy of this remarkable restaurant, offering a delightful dining experience for our guests. Our focus has always been on creating a positive and bright space where guests feel welcomed and inspired.</p>
+        <p className="p__opensans text-center">In 2023, Reverie came to life in Ottawa as an upscale breakfast haven. This picturesque establishment blends elegance with the joy of comfort food. Reverie has become a place where breakfast transcends the ordinary. Our history is a testament to her vision, with each day adding to the legacy of this remarkable restaurant, offering a delightful dining experience for our guests. Our focus has always been on creating a positive and bright space where guests feel welcomed and inspired.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
